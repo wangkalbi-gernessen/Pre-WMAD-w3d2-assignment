19cf4cac18b11951eaae67f3dda70f0f30f333d9
wangkalbi-gernessen <guizhou08chishui@gmail.com>
2020-08-18 14:46:34 -0700

    deleted unnecessary variable

--- function3.js
+++ function3.js
@@ -1,14 +1,11 @@
 // write a function that console logs each item of the array one line at a time
 
 let example = [1, 2, 3, 4, 'five', 6, 7];
-let arrays = [];
 
 const logItems = function (array) {
   for(let i = 0; i < array.length; i++){
-    arrays += array[i];
+    console.log(array[i]);
   }
-  return arrays.join(',');
 };
 
-let result = logItems(example);
-console.log(result);
+logItems(example);
