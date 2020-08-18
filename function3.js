34febcc220308f73040b30d7cad5ac6544b53291
wangkalbi-gernessen <guizhou08chishui@gmail.com>
2020-08-18 10:01:39 -0700

    added function3

--- function3.js
+++ function3.js
@@ -2,9 +2,11 @@
 
 let example = [1, 2, 3, 4, 'five', 6, 7];
 
-const logItems = function () {
-
+const logItems = function (array) {
+  for(let i = 0; i < array.length; i++){
+    console.log(array[i]);
+  }
 };
 
-logitems(example);
+logItems(example);
 
