efe71303c44de9e86f630ce0205ad0dd1d268dcc
wangkalbi-gernessen <guizhou08chishui@gmail.com>
2020-08-18 10:18:53 -0700

    added function 6 and 7

--- function6.js
+++ function6.js
@@ -3,8 +3,9 @@ let food = ['apple', 'orange', 'cake', 'carrots', 'pasta', 'rice', 'eggs'];
 
 let numbers = ['1', '2', '3', '4'];
 
-const addTheLast = function () {
-
+const addTheLast = function (array1, array2) {
+  food = numbers[3].push;
+  console.log(food);
 };
 
 addTheLast(food, numbers);
--- function7.js
+++ function7.js
@@ -1,10 +1,9 @@
 // write a function that joins the array items together into one string, and log it to the console
 let brokenSentence = ['I', 'am', 'a', 'broken', 'sentence'];
 
-
-
-const joinSentence= function () {
-
+const joinSentence= function (String) {
+  let result = String.join(' ');
+  console.log(result);
 };
 
 joinSentence(brokenSentence);
