c4e84f9fb6dbedf8dd038e7c637b4ef05c6bbe74
wangkalbi-gernessen <guizhou08chishui@gmail.com>
2020-08-18 11:21:31 -0700

    modified function2

--- function2.js
+++ function2.js
@@ -6,8 +6,12 @@ let example = [1, 2, 3, 4, 'five', 6, 7];
 let example2 = [1, 2];
 
 const logThird= function (str) {
-  console.log(str);
-  if(str.)
+  // console.log(str.length);
+  if(str.length < 3){
+    console.log("There is no third item");
+  }else {
+    console.log(str[2]);
+  }
 };
 
 logThird(example);
